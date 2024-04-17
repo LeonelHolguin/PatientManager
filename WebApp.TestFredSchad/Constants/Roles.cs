08abae95896a5986f510759b4b9dcb6383984437
Leonel Antonio Holguín Pujóls <leonel.holguin@ulinksolution.com>
2024-04-17 19:00:25 -0400

    Fix(Roles): Se resuelve problema generado en una mal escritura del rol asistente

--- WebApp.TestFredSchad/Constants/Roles.cs
+++ WebApp.TestFredSchad/Constants/Roles.cs
@@ -4,6 +4,6 @@
     {
         public const string Administrator = "Administrator";
 
-        public const string Assistant = "Assitant";
+        public const string Assistant = "Assistant";
     }
 }
